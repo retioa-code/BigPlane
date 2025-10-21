4013c739341666bf8477137c6bc15f3b40634d37
retioa-code <retioa@gmail.com>
2025-10-21 17:09:14 +0800

    Delete Src directory

--- Src/1.UavVisionPlatform/9.Mapper/Mapper.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "Mapper.h"
-#include "../../0.VisionGISAIEngine/1.Common/EngineFunction.h"
-#include "../../0.VisionGISAIEngine/6.GIS/GISHelper.h"
-
-using namespace UavVisionPlatform;
-using namespace VisionGISAIEngine;

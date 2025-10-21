2ead41b5bcedad58733636a3b34d5e15e2e411b6
retioa-code <retioa@gmail.com>
2025-10-21 16:49:02 +0800

    Add files via upload

---
+++ Src/0.VisionGISAIEngine/1.Common/MacroEnumStruct.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+// https://blog.csdn.net/anlian523/article/details/102768496
+#ifdef WIN32
+#define __FILENAME__ (strrchr("\\" __FILE__, '\\') + 1)
+#else
+#define __FILENAME__ (strrchr("/" __FILE__, '/') + 1)
+#endif
+
+#ifdef WIN32
+#define VGA_API __declspec(dllexport)
+#else
+#define VGA_API __attribute__((visibility("default")))
+#endif
+
+#ifdef WIN32
+#define UVP_API __declspec(dllexport)
+#else
+#define UVP_API __attribute__((visibility("default")))
+#endif
+
+enum RunningStatusEnum {
+    Debugging = 0,  // 室内调试
+    FlyingTest = 1 // 室外飞测
+};
+
+struct StdVector2 {
+    StdVector2() {
+    }
+
+    StdVector2(double x, double y) {
+        X = x;
+        Y = y;
+    }
+
+    double X = 0;
+    double Y = 0;
+};
+
+// 按习惯顺序表示：经纬高，横滚角俯仰角偏航角
+struct VGA_API StdVector3 {
+    StdVector3() {
+    }
+
+    StdVector3(double x, double y, double z) {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    double X = 0;
+    double Y = 0;
+    double Z = 0;
+};
---
+++ Src/0.VisionGISAIEngine/6.GIS/GISHelper.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "../1.Common/HeaderFiles.h"
+#include "../1.Common/MacroEnumStruct.h"
+
+namespace VisionGISAIEngine {
+
+    VGA_API StdVector2 CalcGroundToAir(double ground_height, StdVector3 pos_posture);
+
+    VGA_API void PixelToGeography(int pixel_col, int pixel_row, double &geography_x, double &geography_y, double geo_transform[6]);
+
+    VGA_API void GeographyToPixel(double geography_x, double geography_y, int &pixel_col, int &pixel_row, double geo_transform[6]);
+
+    VGA_API bool ConvertGauss6ToLLH(double gauss_x, double gauss_y, double &out_longitude_angle, double &out_latitude_angle);
+
+    VGA_API bool ConvertLLHToGauss6(double longitude_angle, double latitude_angle, double &out_gauss_x, double &out_gauss_y, int iBand = -1);
+
+    VGA_API void ConvertLLHToGauss3(double longitude, double latitude, double &gauss3_x, double &gauss3_y);
+
+    VGA_API void ConvertGauss3ToLLH(double gauss3_x, double gauss3_y, double &longitude, double &latitude);
+
+    VGA_API double GetElevation(double longitude, double latitude, Mat &dem_mat, double geo_transform[6]);
+
+    VGA_API void SavePositioningPoint(QString shape_file_path, QList<QMap<QString, QString>> point_list);
+
+    VGA_API void ConvertGSDToDegrees(double gsd, double latitude, double &gsdLongitude, double &gsdLatitude);
+
+// 判断偏航角 yaw 是否在横、纵两个坐标轴的正负 yaw_tolerance 度以内
+    VGA_API bool IsYawWithinAxesRange(double yaw, double yaw_tolerance);
+
+    VGA_API void PointListToShp(QList<QPair<QString, QPointF>> point_list, QString &shp_file_path);
+
+    VGA_API void ConvertFileNameToShp(QString &dir_path, QString &file_suffix, QString &shp_file_path);
+
+    VGA_API bool IsGaussKruger3Degree(GDALDataset* dataset);
+
+    VGA_API bool IsWGS84(GDALDataset* dataset);
+
+
+}
